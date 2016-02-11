bed24771d5e06369b92bc6f0ca912c9c67d0ebf1
Caio Fischer <caiofis.cf@gmail.com>
2016-02-11 09:30:31 -0200

    Create a example with a random path and odometry

--- example_random.py
+++ example_random.py
@@ -9,7 +9,7 @@ robot.setOdometry(True)                 #configure its odometer
 robot.setOdometryVariance(0.4)
 robot.sim_RandomPath(1,1000)            #run in a random path
 speed , angle =  robot.Odometry()       #reads the sensors
-robot2 = vehicle.Vehicle(1,50)          #create a second model
+robot2 = vehicle.Vehicle()              #create a second model
 robot2.sim_Path(speed,angle)            #run it in the path read by odometry
 
 #show the paths
